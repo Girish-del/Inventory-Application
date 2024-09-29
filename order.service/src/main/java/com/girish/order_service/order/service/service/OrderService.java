23932afb680f51010ad801ef37800e5e5d0cc91c
root <root@srv589524.hstgr.cloud>
2024-09-29 19:23:33 +0000

    Pushing code to new repo

--- order.service/src/main/java/com/girish/order_service/order/service/service/OrderService.java
+++ order.service/src/main/java/com/girish/order_service/order/service/service/OrderService.java
@@ -2,19 +2,24 @@ package com.girish.order_service.order.service.service;
 
 import com.girish.order_service.order.service.client.InventoryClient;
 import com.girish.order_service.order.service.dto.OrderRequest;
+import com.girish.order_service.order.service.event.OrderPlacedEvent;
 import com.girish.order_service.order.service.model.Order;
 import com.girish.order_service.order.service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest){
 
@@ -30,6 +35,12 @@ public class OrderService {
 
             // save order to OrderRepository
             orderRepository.save(order);
+
+            // Send message to kafka topic
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed",orderPlacedEvent);
+            log.info("End - Sent OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
         }
         else{
             throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
